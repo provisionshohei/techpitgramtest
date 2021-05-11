622def9bc9b970ca8054386cab0a3b97d9da0c47
provisionshohei <provisioninc@outlook.jp>
2021-05-11 17:19:09 +0900

    first commit

---
+++ config/initializers/blocked_hosts.rb
@@ -0,0 +1,6 @@
+Rails.application.configure do
+  config.hosts << "localhost"
+  config.hosts << "local.com"
+  config.hosts << "instagram.local.com"
+  config.hosts << "instagram.herokuapp.com"
+end
